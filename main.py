8e2e4d4414b17ed3d025296119bdf99d06a3fced
panaiotuzunov <panaiot.uzunov@gmail.com>
2025-01-19 19:20:25 +0200

    add circleshape.py, add player.py, update constants.py, update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting asteroids!")
@@ -7,12 +8,21 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player1 = Player(x, y)
+
+
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(0)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000 # може да се наложи да се смени с +=
 
 if __name__ == "__main__":
     main()
